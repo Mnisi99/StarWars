0b0a90ba82be521ba30cac30f7a3bf94289accea
Asemahle Mnisi <asemahlemahle663@gmail.com>
2026-02-04 10:18:16 +0200

    Added the footer and some info..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,16 @@ class Footer extends React.Component {
 
     render() {
         return <>
-                    <footer className="container-fluid bg-dark text-white footer-cont mt-5">
+                    <footer className="container-fluid bg-dark text-white footer-cont mt-5 p-4">
+                        <div className="w-25 h-100">
+                            <ul>
+                                <li><h5>Contact Details</h5></li>
+                                <li>asemahleasemahle849@gmail.com</li>
+                                <li>064-9388-268 / 067-3463-533</li>
+                            </ul>
+                        </div>
+                        <div className="w-25"></div>
+                        <div className="w-25"></div>
                     </footer> 
                 </>
     }
