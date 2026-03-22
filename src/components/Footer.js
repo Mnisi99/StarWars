ee522e61845d7b4a3e3e9180c8afb842540b82f6
Asemahle Mnisi <asemahlemahle663@gmail.com>
2026-03-22 13:55:12 +0200

    Updated the content of the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,10 @@ class Footer extends React.Component {
     render() {
         return <>
                     <footer className="container-fluid bg-dark text-white footer-cont mt-5 p-4">
-                        <div className="w-25"></div>
+                        <div className="w-25">
+                            <h4>MovieApp</h4>
+                            
+                        </div>
                         <div className="w-25 h-100">
                             <ul>
                                 <li><h5>Contact Details</h5></li>
@@ -13,7 +16,13 @@ class Footer extends React.Component {
                                 <li>064-9388-268 / 067-3463-533</li>
                             </ul>
                         </div>
-                        <div className="w-25"></div>
+                        <div className="w-25">
+                            <ul>
+                                <li>Check Jobs</li>
+                                <li>News</li>
+                                <li>About us</li>
+                            </ul>
+                        </div>
                     </footer> 
                 </>
     }
